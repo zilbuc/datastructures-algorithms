f444eae2874d7ae9360150b1dd891b3b58239b25
Zilvinas Bucys <zilbuc@gmail.com>
2018-12-22 23:48:39 +0200

    queue from stack

--- stacks & queues.js
+++ stacks & queues.js
@@ -200,3 +200,51 @@ queue.enqeueu(99);
 //queue.printQueue();
 //queue.dequeue();
 //queue.peek();
+
+/***********************
+ ******  Creating A Queue from Stack (or 2 arrays)
+ ************************/
+
+class StackyQueue {
+  constructor() {
+    this.first = [];
+    this.last = [];
+  }
+
+  enqueue(value) {
+    const ln = this.first.length;
+
+    for (let i = 0; i < ln; i++) {
+      this.last.push(this.first.pop());
+    }
+    this.last.push(value);
+    return this;
+  }
+
+  dequeue() {
+    const ln = this.last.length;
+
+    for (let i = 0; i < ln; i++) {
+      this.first.push(this.last.pop());
+    }
+    this.first.pop();
+    return this;
+  }
+
+  peek() {
+    if (this.first.length === 0) {
+      return this.last[0];
+    }
+    return this.first[this.first.length - 1];
+  }
+
+}
+
+const myQueue = new StackyQueue();
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+myQueue.dequeue();
+myQueue.enqueue(4);
+//myQueue.dequeue();
+myQueue.peek();
