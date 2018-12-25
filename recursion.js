d2154ee8d25f13df1fa68bc4d6e711e91257909c
Zilvinas Bucys <zilbuc@gmail.com>
2018-12-26 00:57:55 +0200

    recursion - fibonacci

--- recursion.js
+++ recursion.js
@@ -19,3 +19,28 @@ function findFactorialIterative(number) {
 
 findFactorialRecursive(5);
 //findFactorialIterative(5);
+
+/***********************
+ ******  Factorial
+ ************************/
+
+function fibonacciIterative(n) {
+  let array = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    array.push(array[i - 2] + array[i - 1]);
+  }
+  return array[n];
+}
+
+fibonacciIterative(8);
+
+function fibonacciRecursive(n) {
+
+  if (n < 2) {
+    return n;
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+//fibonacciRecursive(7);
